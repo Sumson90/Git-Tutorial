dd1d47623ad29d880ee2328b1a786c9ed444ac70
Tomasz Sumkowski <Sumkowski.Tomasz@gmail.com>
2023-01-19 17:43:50 +0100

    feat [TASK-3456] Commit form IntelliJ

--- Dog2.java
+++ Dog2.java
@@ -1,15 +1,5 @@
 public class Dog2 {
     public static void  name() {
         System.out.println("fsdf");
-    }  public static void  name2() {
-        System.out.println("fsdf");
-    }  public static void  name3() {
-        System.out.println("fsdf");
-    } public static void  name4() {
-        System.out.println("fsdf");
-    }  public static void  name7() {
-        System.out.println("fsdf");
-    }  public static void  name6() {
-        System.out.println("fsdf");
     }
 }
